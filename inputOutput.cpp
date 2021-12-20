7b15b649719ca949e12f77d28850647f9e456692
Vaibhav Vyas <52570863+GenxTheGamer@users.noreply.github.com>
2021-12-20 15:17:22 +0530

    Added Files

---
+++ inputOutput.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << "Sum of Two Numbers";
+
+    int num1; //Number 1
+    int num2; //Number 2
+
+    cin >> num1; //Taking Input from user for Number 1
+    cin >> num2; //Taking Input from user for Number 2
+
+    int sum = num1 + num2;
+
+    cout << "Sum of Two numbers inserted is : " << sum << endl;
+
+    return 0;
+}
